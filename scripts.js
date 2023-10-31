d115d8283e2f65c8119b39158291dd4ced105734
cuarty <dibartolo.fx@gmail.com>
2023-10-31 10:17:25 -0300

    edicionFormulario

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const tiket = 2500;
+
+let descEstudiante = 0.80;
+let descJubilado = 0.50;
+let descVisitante = 0.15;
+
+const cant = document.getElementById("cantidad");
+const cat = document.getElementById("categoria");
+const totalAPagar = document.getElementById("resultado");
+const boton = document.getElementById("resumen");
+
+function calcularPago() {
+    let total = cant.value * tiket;
+
+    switch (cat.value) {
+        case "estudiante":
+            total = total - (total * descEstudiante);
+            totalAPagar.textContent = `Total a Pagar: $ ${total}`
+            break;
+
+        case "jubilado":
+            total = total - (total * descJubilado);
+            totalAPagar.textContent = `Total a Pagar: $ ${total}`
+            break;
+
+        case "visitante":
+            total = total - (total * descVisitante);
+            totalAPagar.textContent = `Total a Pagar: $ ${total}`
+            break;
+    
+    }
+
+    
+}
+
+
+boton.addEventListener("click", (e) =>{
+    e.preventDefault()
+    calcularPago()
+});
